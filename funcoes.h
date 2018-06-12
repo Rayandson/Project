2f1383c37bb6d82e1758a7bb6c9140cfc1e267d3
Rayandson <rayandson_silva@hotmail.com>
2018-06-12 20:33:41 -0300

    funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,9 @@
+void lerMatriz(float mat[20][20], int m, int n);
+
+void somarMatrizes(float a[20][20], float b[20][20], float c[20][20], int m, int n);
+
+void subtrairMatrizes(float a[20][20], float b[20][20], float c[20][20], int m, int n);
+
+void escreverMatriz(float mat[20][20], float m, int n);
+
+void menu(void);
