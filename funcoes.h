9f603289d11f15950a36d8c96a21fa5ce083f3ea
Rayandson <rayandson_silva@hotmail.com>
2018-06-23 16:07:32 -0300

    Atual funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -4,6 +4,8 @@ void somarMatrizes(float a[20][20], float b[20][20], float c[20][20], int m, int
 
 void subtrairMatrizes(float a[20][20], float b[20][20], float c[20][20], int m, int n);
 
+void multiplicarMatrizes(float a[20][20], float b[20][20], float c[20][20], int l, int m, int n);
+
 void escreverMatriz(float mat[20][20], float m, int n);
 
 void menu(void);
