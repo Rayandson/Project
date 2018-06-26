b42b4941c5d3dae1931d18261864b8abedd73c48
Rayandson <rayandson_silva@hotmail.com>
2018-06-26 18:38:14 -0300

    Atual main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main(void)
 {
        int opcao, numLA = 21, numLB =21, numCA =21, numCB =21;
-       float m1[20][20], m2[20][20], m3[20][20];
+       float A[20][20], B[20][20], C[20][20];
 
        srand(time(NULL));
 
@@ -23,59 +23,71 @@ int main(void)
        }
         break;
     case 2:
-      printf("Informe o numero de linhas da matriz B: ");
+       while((numLB > 20) || (numCB > 20)){
+       printf("Informe o numero de linhas da matriz B (Limite = 20 linhas): ");
        scanf("%d", &numLB);
-       printf("Informe o numero de colunas da matriz B: ");
+       printf("Informe o numero de colunas da matriz B (Limite = 20 colunas): ");
        scanf("%d", &numCB);
+       }
       break;
     case 3:
-      for(int l=0;l<20;l++){
-    for(int c=0;c<20;c++){
-        m1[l][c] = (rand()% 20);
+      for(int l=0;l<numLA;l++){
+        for(int c=0;c<numCA;c++){
+        A[l][c] = (rand()% 20);
     }
 }
       break;
     case 4:
-            for(int l=0;l<20;l++){
-    for(int c=0;c<20;c++){
-        m2[l][c] = (rand()% 20);
+        for(int l=0;l<numLB;l++){
+            for(int c=0;c<numCB;c++){
+            B[l][c] = (rand()% 20);
     }
 }
       break;
     case 5:
         printf("informe os valores da matriz A:\n");
-           lerMatriz(m1, numLA, numCA);
+           lerMatriz(A, numLA, numCA);
       break;
     case 6:
       printf("informe os valores da matriz B:\n");
-           lerMatriz(m2, numLB, numCB);
+           lerMatriz(B, numLB, numCB);
       break;
     case 7:
-      somarMatrizes(m1, m2, m3, numLA, numCA);
+      if((numLA == numLB) && (numCA == numCB)){
+      somarMatrizes(A, B, C, numLA, numCA);
+      }
+      else {
+        printf("As matrizes possuem tamanhos diferentes portanto a soma nao pode ser efetuada. \n");
+      }
       break;
     case 8:
-      subtrairMatrizes(m1, m2, m3, numLA, numCA);
+      if((numLA == numLB) && (numCA == numCB)){
+      subtrairMatrizes(A, B, C, numLA, numCA);
+      }
+      else {
+        printf("As matrizes possuem tamanhos diferentes portanto a subtração nao pode ser efetuada. \n");
+      }
       break;
     case 9:
-      multiplicarMatrizes(m1, m2, m3, numLA, numCB, numCA);
+      multiplicarMatrizes(A, B, C, numLA, numCB, numCA);
       break;
     case 10:
-      escreverMatriz(m1, numLA, numCA);
+      escreverMatriz(A, numLA, numCA);
       break;
     case 11:
-      escreverMatriz(m2, numLB, numCB);
+      escreverMatriz(B, numLB, numCB);
       break;
     case 12:
-      escreverMatriz(m3, numLA, numCB);
+      escreverMatriz(C, numLA, numCB);
       break;
     case 13:
-      printf("fat  = %d\n");
+      printf("Opcao indisponivel no momento. \n");
       break;
     case 14:
-      printf("fat  = %d\n");
+      printf("Opcao indisponivel no momento. \n");
       break;
     case 15:
-      printf("fat  = %d\n");
+      printf("Opcao indisponivel no momento. \n");
       break;
     case 16:
       exit(0);
